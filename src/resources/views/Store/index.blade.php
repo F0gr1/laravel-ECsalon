8502fcfaa0b39f50e3633e705fe18d02bc834361
F0gr1 <gami20010314@gmail.com>
2023-06-19 11:36:32 +0900

    update デザイン変更

--- src/resources/views/Store/index.blade.php
+++ src/resources/views/Store/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Document</title>
-</head>
+@extends('layouts/header')
+@section('content')
+    
 <body>
     <div class="container">
         <div class="row">
@@ -15,7 +8,7 @@
             <div class="card col-4 p-4">
                 <img src="{{asset('storage/'.$store->icon)}}" class="card-img-top" style="width:340px; height:340px;">
                 <div class="card-body">
-                    <h5 class="card-title">{{$store->name}}</h5>
+                    <h5 class="card-title text-primary">{{$store->name}}</h5>
                     <p class="card-text">{{$store->description}}</p>
                 </div>
                 <ul class="list-group list-group-flush">
@@ -25,7 +18,7 @@
                     <li class="list-group-item">住所:{{$store->adress}}</li>
                 </ul>
                 <div class="card-body">
-                    <a href="#" class="card-link">詳細</a>
+                    <a href="/{{$store->id}}" class="card-link">詳細</a>
                 </div>
             </div>
             @endforeach
@@ -33,3 +26,4 @@
     </div>
 </body>
 </html>
+@endsection('content')    
